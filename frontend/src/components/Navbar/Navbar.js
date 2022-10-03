abac9e10e4d04fca6e6b64cd1d1217b004162094
ChandraShekar2001 <pvchandu07@gmail.com>
2022-10-03 12:43:40 +0530

    Added Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,32 +1,64 @@
 import React from "react";
 import classes from "./Navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = (props) => {
+  const navigate = useNavigate();
+  const onClickHandler = (e) => {
+    navigate("/products");
+  };
   return (
     <>
-      <div className={classes.navbar}>
+      <nav className={classes.nav}>
         <div className={classes.logo}>
-          <ul className={classes["remove-bullets"]}>
-            <li>ECOMMERCE</li>
-          </ul>
+          <Link to="/" className={`${classes.links} ${classes["font-poppin"]}`}>
+            ECOMMERCE
+          </Link>
         </div>
-        <div className={classes["nav-items"]}>
-          <ul className={classes["remove-bullets"]}>
-            <input type="text" />
-            <li>
-              <Link to="#">Home</Link>
-            </li>
-            <li>
-              <Link to="#">About</Link>
-            </li>
-            <li>
-              <Link to="#">Contact</Link>
-            </li>
-          </ul>
+
+        <div>
+          <button
+            className={classes["dropdown-content"]}
+            onClick={onClickHandler}
+          >
+            Products
+          </button>
         </div>
-      </div>
-      <div>welcome { props.user.name}</div>
+
+        <div className={classes.search}>
+          <input
+            className={classes["search-header"]}
+            type="search"
+            name="search"
+            placeholder="Search..."
+          />
+        </div>
+
+        <ul className={classes["nav-list"]}>
+          <li className={`${classes.item} ${classes["font-poppin"]} `}>
+            <Link to="/home" className={`${classes.links}`}>
+              Home
+            </Link>
+          </li>
+          <li className={`${classes.item} ${classes["font-poppin"]}`}>
+            <Link to="/contact" className={`${classes.links}`} id="contact">
+              Contact
+            </Link>
+          </li>
+          <li className={classes.item}>
+            <Link>
+              <FontAwesomeIcon icon={faCartPlus} className={classes.black} />{" "}
+            </Link>
+          </li>
+          <li className={classes.item}>
+            <Link>
+              <FontAwesomeIcon icon={faUser} className={classes.black} />{" "}
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
